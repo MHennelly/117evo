63f58ffee5d2da60f319b427174752f9f0bf407b
Michael Hennelly <michaelthehennelly@gmail.com>
2018-12-04 12:03:26 -0800

    asdf

--- Server1.py
+++ Server1.py
@@ -1,17 +1,18 @@
-import socket                
-  
-s = socket.socket()          
+import socket
+from simulation import Simulation
+
+s = socket.socket()
 print ("Socket successfully created")
-   
-port = 12345                
-  
-s.bind(('', port))         
-print ("socket binded to %s" %(port)) 
-  
-s.listen(5)      
-print ("socket is listening")            
-  
-c, addr = s.accept()      
+
+port = 12345
+
+s.bind(('', port))
+print ("socket binded to %s" %(port))
+
+s.listen(5)
+print ("socket is listening")
+
+c, addr = s.accept()
 print ('Got connection from', addr)
 
 DataRecv= ""
@@ -19,9 +20,12 @@ DataRecv= ""
 x = "yes"
 while x != "no":
       DataRecv = c.recv(1024).decode()
-      Tx = DataRecv[DataRecv.find("(")+1:DataRecv.find(",")]
-      Ty = DataRecv[DataRecv.find(",")+1:DataRecv.find(")")]
-      a[2] = {}
+      Tx = int(DataRecv[DataRecv.find("(")+1:DataRecv.find(",")])
+      Ty = int(DataRecv[DataRecv.find(",")+1:DataRecv.find(")")])
+      a = Simulation(Tx, Ty)
+      a.run()
+      vel, ang = a.getValues()
+      a[2] = {vel, ang}
       DataSend = "Target was hit at" + DataRecv + " with velocity " + str(a[0] + "and angle " + str(a[1])
       c.send(DataSend.encode())
       x = c.recv(1024).decode()
